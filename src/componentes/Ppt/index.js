5bdab670297c5ca728067623e6cc4b86b8777d96
Toni Fernandez <tonifenoy78@gmail.com>
2022-07-27 17:32:44 +0200

    comenzado, muy verde

---
+++ src/componentes/Ppt/index.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react'
+import './style.css';
+
+export default class Ppt extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            
+        }
+    }
+
+        render() {
+        
+        return (
+            <div id="ppt">
+                {/* <h1>Vamos a jugar a un juego</h1> */}
+                <div id=''>
+                    
+                </div>
+                <div id=''>
+                    
+                </div>
+                
+            </div>
+        )
+    }
+}
---
+++ src/componentes/Seleccion/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import './style.css';
+import Piedra from '../../images/piedra.png'
+import Papel from '../../images/papel.png'
+import Tijeras from '../../images/tijeras.png'
+
+const Seleccion = ({}) => {
+
+    return (
+        <div id="palabra">{palabra}</div>
+    )
+}
+
+export default Seleccion
